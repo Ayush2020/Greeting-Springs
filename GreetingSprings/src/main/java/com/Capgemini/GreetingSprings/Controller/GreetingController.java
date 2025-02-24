9b7bc72acacd89e5957486a749fb8cc33446b46b
Ayush2020 <ayushgiri2021itc@gmail.com>
2025-02-24 12:18:08 +0530

    [Ayush] add . Greeting message using Service Layer

--- GreetingSprings/src/main/java/com/Capgemini/GreetingSprings/Controller/GreetingController.java
+++ GreetingSprings/src/main/java/com/Capgemini/GreetingSprings/Controller/GreetingController.java
@@ -1,14 +1,30 @@
 package com.Capgemini.GreetingSprings.Controller;
 
+import com.Capgemini.GreetingSprings.Service.ServiceLayer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class GreetingController {
+public class GreetingController extends ServiceLayer {
 
     @GetMapping("/greeting")
     public ResponseEntity<String> getGreeting(@RequestParam(value="name", defaultValue = "World") String name){
         return new ResponseEntity<>(String.format("Hello, %s", name), HttpStatus.OK);
+    }
+
+    @PostMapping("/greeting/{name}")
+    public ResponseEntity<String> postGreeting(@PathVariable("name") String name){
+        return new ResponseEntity<>(String.format("Create, %s", name), HttpStatus.OK);
+    }
+
+    @PutMapping("/greeting/put/{name}")
+    public ResponseEntity<String> putGreeting(@PathVariable("name") String name){
+        return new ResponseEntity<>(String.format("Update, %s", name), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/greeting/delete/{name}")
+    public ResponseEntity<String> deleteGreeting(@PathVariable("name") String name){
+        return new ResponseEntity<>(String.format("Delete, %s", name), HttpStatus.OK);
     }
 }
---
+++ GreetingSprings/src/main/java/com/Capgemini/GreetingSprings/Service/ServiceLayer.java
@@ -0,0 +1,7 @@
+package com.Capgemini.GreetingSprings.Service;
+
+public class ServiceLayer {
+    protected void displayMessage(){
+        System.out.println("Hello World");
+    }
+}
